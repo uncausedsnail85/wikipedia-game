802e21e9d00759b141634e7527049ab098f240bc
Bryan Ang <bryan.ang>
2022-12-03 20:53:51 -0500

    added a generic A* search with a null heuristic. Meaning will behave like a BFS

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 import wikipedia
 import sys
-from queue import Queue
+from queue import Queue, PriorityQueue
 import time
 
 # returns a list of page titles, the path
@@ -10,7 +10,7 @@ def wikiGameDFS(initialPageTitle, destinationPageTitle):
     if initialPageTitle == destinationPageTitle:
         return [initialPageTitle]
 
-    # start timer
+    # start timer, ideally should be commented out
     start = time.time()
 
     # search node of a BFS graph
@@ -45,7 +45,7 @@ def wikiGameDFS(initialPageTitle, destinationPageTitle):
         try:
             succList = wikipedia.page(curr_node[0], auto_suggest=False).links
         except Exception as e:
-            print("e")
+            print(e)
 
         # add each link into queue
         # And check if link is already in explored set to
@@ -55,3 +55,71 @@ def wikiGameDFS(initialPageTitle, destinationPageTitle):
                 succNodePath.append(succ)
                 succNode = (succ, succNodePath)
                 frontier.put(succNode)
+
+##########################################################
+
+# Given a page, return the distance between the category
+# of the given page and the category of the dest page.
+# Wikipedia organizes categories with subcategories,
+# leading to an almost tree like structure (accurately
+# it is a partially ordered set). By using the distance
+# to the target category, we can use it as a heuristic.
+def heuristic(pageTitle, destinationTitle):
+    return 0 # temp
+
+def wikiGameAStar(initialPageTitle, destinationPageTitle, heuristic):
+
+    # source and dest are the same page
+    if initialPageTitle == destinationPageTitle:
+        return [initialPageTitle]
+
+    # start timer, ideally should be commented out
+    start = time.time()
+
+    # the search node of a A* search graph
+    initialNode = (initialPageTitle, [initialPageTitle])
+    # Init priority queue for, keeping track of lowest heuristic
+    frontier = PriorityQueue(maxsize=0)
+    frontier.put((heuristic(initialNode[0], destinationPageTitle), initialNode))
+    # explored set
+    explored = [] #List of titles (string)
+
+    # loop forever until: queue is empty or destination page is reached
+    while(True):
+        print("elapsed time: ", round(time.time()-start, 1))
+
+        # if queue is empty, no further 
+        if frontier.empty(): return None
+        frontierPop = frontier.get()
+        curr_node = frontierPop[1]
+
+        # If a link has been explored before, continue.
+        # This happens when a link is added multiple times to the
+        # queue by different pages.
+        if (curr_node[0] in explored): continue
+
+        # Explore the link by adding to explored set
+        explored.append(curr_node[0])
+
+        # Test if link is destination
+        if (curr_node[0] == destinationPageTitle): return curr_node[1]
+        print("frontier.get()", frontierPop)
+        print("curr_node: ", curr_node)
+
+        # Get links in page as successors
+        try:
+            succList = wikipedia.page(curr_node[0], auto_suggest=False).links
+        except Exception as e:
+            print(e)
+
+        # add each link into queue
+        # And check if link is already in explored set to
+        for succ in succList:
+            if (succ not in explored):
+                succNodePath = curr_node[1].copy()
+                succNodePath.append(succ)
+                succPathCost = 1 + frontierPop[0]
+                succNode = (succ, succNodePath)
+                if (succNode[0] not in explored):
+                    frontier.put((succPathCost + heuristic(succNode[0], destinationPageTitle), succNode))
+
--- wikiGameDriver.py
+++ wikiGameDriver.py
@@ -4,9 +4,18 @@ import time
 startTime = time.time()
 startPage = "Everything Everywhere All at Once"
 destPage = "Nihilism"
+print("Searching for path between", startPage, "and", destPage)
+
+print("Starting a BFS search")
 finalPath  = search.wikiGameDFS(startPage, destPage)
 timeTaken = round(time.time()-startTime, 1)
+print("The found shortest path between", startPage, "and", destPage, "is: ")
+print(finalPath)
+print("It took", timeTaken, "seconds")
 
+print("Starting a BFS search")
+print(search.wikiGameAStar(startPage, destPage, search.heuristic))
+timeTaken = round(time.time()-startTime, 1)
 print("The found shortest path between", startPage, "and", destPage, "is: ")
 print(finalPath)
 print("It took", timeTaken, "seconds")
