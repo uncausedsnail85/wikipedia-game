251850ead5753dc68256494dd607b1dcf30abc33
Bryan Ang <bryan.ang>
2022-12-06 14:25:30 -0500

    added first heursitic for number of overlapping categories

--- search.py
+++ search.py
@@ -64,10 +64,37 @@ def wikiGameDFS(initialPageTitle, destinationPageTitle):
 # leading to an almost tree like structure (accurately
 # it is a partially ordered set). By using the distance
 # to the target category, we can use it as a heuristic.
-def heuristic(pageTitle, destinationTitle):
-    return 0 # temp
+def numberOfSameCategoriesHeuristic(pageTitle, destinationCategories):
+    try:
+        sourceCats = wikipedia.page(pageTitle, auto_suggest=False).categories
+    except Exception as e:
+        print(e)
+        return sys.maxsize
+
+    # sourceCats = wikipedia.page(pageTitle, auto_suggest=False).categories
+    # print("find heuristic for Source page:", pageTitle)
+    # todo: bring this call outside of the function
+    # destCats = wikipedia.page(destinationTitle, auto_suggest=False).categories 
+    destCats = destinationCategories.copy()
+    # print("DestCats:", destCats)
+    # return number of matching categories
+    numberMatches = 0
+    for i in sourceCats:
+        for j in destCats:
+            # print("j:", j)
+            if i == j:                
+                numberMatches =+ 1
+                destCats.remove(j)
+    # print("Done, number of matches:", numberMatches)
+    return numberMatches
+
+def nullHeuristic(pageTitle, destinationTitle):
+    return 0 
 
 def wikiGameAStar(initialPageTitle, destinationPageTitle, heuristic):
+
+    # pre-fetching destination page categories
+    destinationPageCategories =  wikipedia.page(destinationPageTitle, auto_suggest=False).categories 
 
     # source and dest are the same page
     if initialPageTitle == destinationPageTitle:
@@ -75,12 +102,13 @@ def wikiGameAStar(initialPageTitle, destinationPageTitle, heuristic):
 
     # start timer, ideally should be commented out
     start = time.time()
+    numberOfExpansions = 0
 
     # the search node of a A* search graph
     initialNode = (initialPageTitle, [initialPageTitle])
     # Init priority queue for, keeping track of lowest heuristic
     frontier = PriorityQueue(maxsize=0)
-    frontier.put((heuristic(initialNode[0], destinationPageTitle), initialNode))
+    frontier.put((heuristic(initialNode[0], destinationPageCategories), initialNode))
     # explored set
     explored = [] #List of titles (string)
 
@@ -91,6 +119,7 @@ def wikiGameAStar(initialPageTitle, destinationPageTitle, heuristic):
         # if queue is empty, no further 
         if frontier.empty(): return None
         frontierPop = frontier.get()
+        numberOfExpansions =+ 1
         curr_node = frontierPop[1]
 
         # If a link has been explored before, continue.
@@ -102,7 +131,9 @@ def wikiGameAStar(initialPageTitle, destinationPageTitle, heuristic):
         explored.append(curr_node[0])
 
         # Test if link is destination
-        if (curr_node[0] == destinationPageTitle): return curr_node[1]
+        if (curr_node[0] == destinationPageTitle): 
+            print("NUMBER OF NODES EXPANDED:", numberOfExpansions)
+            return curr_node[1]
         print("frontier.get()", frontierPop)
         print("curr_node: ", curr_node)
 
@@ -121,5 +152,5 @@ def wikiGameAStar(initialPageTitle, destinationPageTitle, heuristic):
                 succPathCost = 1 + frontierPop[0]
                 succNode = (succ, succNodePath)
                 if (succNode[0] not in explored):
-                    frontier.put((succPathCost + heuristic(succNode[0], destinationPageTitle), succNode))
+                    frontier.put((succPathCost + heuristic(succNode[0], destinationPageCategories), succNode))
 
